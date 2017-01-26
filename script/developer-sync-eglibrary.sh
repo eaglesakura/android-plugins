3b525bb884e6de4668bf50c249c7dc5975ab0cd8
eaglesakura <eagle.sakura@gmail.com>
2017-01-26 12:55:48 +0900

    同期スクリプトを変更

--- script/developer-sync-eglibrary.sh
+++ script/developer-sync-eglibrary.sh
@@ -1,15 +1,35 @@
-#! /bin/sh
-if [ -e eglibrary/.git/ ]; then
-  echo "eglibrary has cache"
-else
-  rm -rf ./eglibrary
-  git clone git@github.com:eaglesakura/eglibrary.git
-fi
+#! /bin/sh -eu
+
+git submodule update --init
+
 
 cd eglibrary/
-git clean -f .
-git checkout -f develop
-git pull origin develop
-chmod 755 ./script/sync-develop.sh
-./script/sync-develop.sh
+repo_sync() {
+    echo "########################################"
+    echo "## sync $1 / $2"
+    echo "########################################"
+    cd $1
+
+    git clean -f
+    git fetch
+    git branch -d $2
+    git checkout -f $2
+    git pull origin $2
+    git branch
+    cd ..
+}
+
+repo_sync "geo-utils" "develop"
+repo_sync "json-wrapper" "develop"
+repo_sync "simple-logger" "develop"
+repo_sync "simple-serialize" "develop"
+repo_sync "simple-utils" "develop"
+repo_sync "junit-support" "develop"
+
+repo_sync "gradle-plugin-utils" "develop"
+repo_sync "plugin-ci-support" "develop"
+repo_sync "plugin-android-dbgenerator" "develop"
+repo_sync "plugin-android-mipmap" "develop"
+repo_sync "plugin-firebase-remoteconfig" "develop"
+
 cd ../
